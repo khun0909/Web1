bdb7231020ba8b6eae354ca537b889991f78fee2
khun0909 <son641124@naver.com>
2023-07-25 21:30:13 +0900

    230725-2

--- 10/js/string1.js
+++ 10/js/string1.js
@@ -1,36 +1,39 @@
-//입력한 텍스트 길이 체크
 
-var str = prompt("문자열을 입력하세요.","");
+// 입력된 데이터 형식에 따른 속성과 메소드
+
+var str = prompt("문자열을 입력하세요", "");
 console.log("입력한 문자열 : " + str);
 
-console.log("입력한 문자열의 길이 : " + str.length); //문자열 객체에 대한 length 프로퍼티
-console.log("특정 위치값의 글자 : " + str.charAt(3)); //문자 객체에 대한 메소드
-console.log("특정 위치값의 글자 : " + str[4]); //문자열에서 특정 위치의 글자를 반환
-console.log("특정 글자의 위치값 출력 : " + str.indexOf('요')); //문자열에서 특정 글자의 위치값을 반환
+console.log("입력한 문자열의 길이 : " + str.length);  // 문자열 객체에 대한 length 프로퍼티
+console.log("특정 위치값의 글자 : " + str.charAt(3));  // 문자열에서 특장 위치의 글자를 반환하는 메소드
+console.log("특정 위치값의 글자 : " + str[3]);  // 문자열에서 특장 위치의 글자를 반환하는 메소드
+console.log("특정 글자의 위치값 출력: : " + str.indexOf('요'));  // 문자열에서 특장 글자의 위치값을 반환하는 메소드
 
-//문자열에서 특정 글자가 몇개이며 위치는 어디인지 반환하는 함수
-function searchChar(str, char){
-    let count = 0; //특정 문자의 개수를 저장할 변수 
-    let pos = new Array();  //특정 글자의 위치값을 저장할 배열
-    
-    for (let i = 0; i < str.length;){
-        if(str===char){
-            count += 1;
+// 문자열에서 특정 글자가 몇개이며 위치는 어디인지 반환하는 함수
+
+function searchChar(str, char) {
+    let count = 0;  // 특정 글자의 개수를 저장할 변수
+    let pos = new Array();  // 특정 글자의 위치값을 저장할 배열
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count += 1
             pos.push(i);
         }
     }
-    return count, pos;
 
+    // return [count, pos];  // 배열로 반환
+    return {tcnt:count, tpos:pos};  // 객체로 반환
 }
 
-const strText = prompt("문장을 입력하세요.", "");
+const strText = prompt("문장을 입력하세요", "");
 const charText = prompt("찾을 문자를 입력하세요.", "");
 
-let count, pos;
-count, pos = searchChar(strText, charText);
+// let [count, pos] = searchChar(strText, charText);
+// console.log("찾는 문자의 개수 : " + count);
+// console.log("찾는 문자의 위치 : " + pos);
 
-console.log("찾는 문자의 개수 :" + count);
-console.log("찾는 문자의 위치 :" + pos);
-
-
+let result = searchChar(strText, charText);
+console.log("찾는 문자의 개수 : " + result.tcnt);
+console.log("찾는 문자의 위치 : " + result.tpos);
 
--- 10/js/string2.js
+++ 10/js/string2.js
@@ -1,11 +1,29 @@
-//1. 사용자로부터 문장을 입력받아 변수에 저장한다.
-//2. 찾을 문자를 입력받아 변수에 저장한다.
-//3. 입력받은 단어가 몇개이며, 위치는 어디인지 작성한다.
 
-var str = prompt("문자열을 입력하세요.","");
+// 1. 사용자로부터 문장을 입력받아 변수에 저장한다.
+// 2. 사용자로부터 찾을 문자(단어)를 입력받아 변수에 저장한다.
+// 3. 입력받은 문장에서 입력받은 문자(단어)가 몇개이며 위치는 어디인지 반환하는 함수를 작성한다.
 
-
-
-console.log("특정 글자의 위치값 출력 : " + str.indexOf('요')); //문자열에서 특정 글자의 위치값을 반환
+// substring(시작위치, 종료위치) : 시작위치부터 종료위치(값미포함)까지의 문자열을 반환
 
 
+// **************************************       
+function countChar(string, chr) {
+    var cnt = 0;
+    var pos = new Array();
+    var len = chr.length;
+
+    for (var i = 0; i < string.length - len + 1; i++) {
+        if (string.substring(i, len + i) == chr) {
+            cnt++;
+            pos.push(i);
+        }
+    }
+    return [cnt, pos];
+}
+
+var string = "가나가나다라마바가나다마바사아가나";
+var chr = "가나다";
+
+let result = countChar(string, chr);
+console.log(result[0]);
+console.log(result[1]);   // result[개수, [위치값1, 위치값2, ...]]
